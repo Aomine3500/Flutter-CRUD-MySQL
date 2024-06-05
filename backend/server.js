ca28699ce35bcf59cc41df41774e8250a2c38101
Ahmed ZNOUDA <42349194+Aomine3500@users.noreply.github.com>
2024-06-05 11:22:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ayza2380mkdir',
+    password: '',
     database: 'gestcomprod'
 });
 
